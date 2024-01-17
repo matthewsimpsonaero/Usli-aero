3136b77bf3ed012b75f0039ba72cd886120791e7
Matthew Simpson <mssimpso@ncsu.edu>
2024-01-17 01:15:27 -0500

    Update motor to L1940X

--- MATLAB/CDR/Motor_Plot.m
+++ MATLAB/CDR/Motor_Plot.m
@@ -1,7 +1,7 @@
 % Matthew Simpson
 % Motor Data Plot
 
-motorData = readmatrix('AeroTech_L1390G.csv');
+motorData = readmatrix('AeroTech_L1940X.csv');
 
 fig = figure();
 fig.Position = [100,100,800,600];
@@ -10,6 +10,26 @@ grid on
 grid minor
 xlabel('Time (s)')
 ylabel('Force (lbf.)')
-title('AeroTech L1390G Thrust Profile')
-yline(312.4844,'g',LineWidth=2)
+title('AeroTech L1940X Thrust Profile')
+yline(435.9,'g',LineWidth=2)
 legend('Thrust Force','Average Thrust')
+
+%% Thrust to weight Measurement
+
+TWR = readmatrix('Thrust_weight_Data.csv');
+
+inital_weight = 45.74+4;
+times = motorData(:,1);
+weight = -1.7427.*times +49.74;
+
+fig2 = figure();
+fig2.Position = [100,100,800,600];
+plot(times,(motorData(:,2)*0.224809)./weight,'b-o',LineWidth=2)
+hold on
+plot(TWR(:,1),TWR(:,3)./TWR(:,2),'g-o',LineWidth=2)
+grid on 
+grid minor
+xlabel('Time (s)')
+ylabel('Thrust To Weight Ratio')
+title('Thrust to Weight Ratio of Full Scale Launch Vehicle')
+legend('Hand Calculation','RocketPy')
